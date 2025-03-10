ab4fd5aded8f1c2f8a9476ac286f4a7e306cf146
Luis <iluisflores@yahoo.com>
2025-03-10 14:25:39 -0500

    created test cases for /musicians GET route

--- index.test.js
+++ index.test.js
@@ -1,22 +1,38 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
+const { json } = require("sequelize");
 
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("Testing musicians endpoint", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+  test("Testing musicians endpoint returns back data in json format", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+  });
 
+  test("Musicians endpoint returns data with expected structure", async () => {
+    const response = await request(app).get("/musicians");
+    const musicians = JSON.parse(response.text);
+    expect(Array.isArray(musicians)).toBe(true);
+    musicians.forEach(musician => {
+        expect(musician).toHaveProperty('id');
+        expect(musician).toHaveProperty('name');
+        expect(musician).toHaveProperty('instrument');
+    });
 
-
-
-    
-})
+});
+});
