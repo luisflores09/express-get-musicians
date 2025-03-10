8180880fb5bc5cc94cba23272b5cec6ab857e4bd
Luis <iluisflores@yahoo.com>
2025-03-10 13:55:59 -0500

    created GET route to return all musicians

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
-
+app.get('/musicians', async (req, res) => {
+    let allMusicians = await Musician.findAll();
+    res.json(allMusicians)
+})
 
 
 
